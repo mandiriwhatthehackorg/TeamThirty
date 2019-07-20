aa8ce9630a761f2ef32924771187be8065dceeb2
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-07-20 23:41:46 +0700

    add introducing page

--- lib/screens/widgets/introducing.dart
+++ lib/screens/widgets/introducing.dart
@@ -4,16 +4,18 @@ class Introducing extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Colors.white,
         body: GestureDetector(
-      onTap: () {
-        Navigator.pushNamed(context, '/SignIn');
-      },
-      child: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/introducing.jpg'),
-                fit: BoxFit.cover)),
-      ),
-    ));
+          onTap: () {
+            Navigator.pushNamed(context, '/SignIn');
+          },
+          child: Container(
+            margin: EdgeInsets.only(top: 50),
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('assets/introducing.jpg'),
+                    fit: BoxFit.cover)),
+          ),
+        ));
   }
 }
