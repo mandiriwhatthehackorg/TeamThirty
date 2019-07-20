6632bf92c17673afcabcd7c85960958e0c0f29e2
Fuadit Muhammad <muhammadfuaditrockz@gmail.com>
2019-07-20 18:04:08 +0700

    first_commit

---
+++ android/app/src/main/java/com/example/pariwisata_mandiri/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.pariwisata_mandiri;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
